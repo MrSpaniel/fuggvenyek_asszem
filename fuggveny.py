0882379c7490f446cd810cdcca816ad4d3af59c7
Fehér Dániel <FeherDaniel@pg.local>
2025-11-12 09:30:34 +0100

    Create fuggveny.py

---
+++ fuggveny.py
@@ -0,0 +1,5 @@
+'''Írj egy programot, amely tartalmaz egy 'osszegzo' nevű függvényt, ami a paraméterként át
+vett listaelemeket (egész számokat) összeadja és az összegükkel tér vissza! A program tartalmazza a függvény hívását is!'''
+
+def osszegzo(szamok):
+    if
